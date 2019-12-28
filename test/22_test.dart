9e6c73715d1463efd861699b2d0762f7c2c1664e
Eric Jain <eric.jain@gmail.com>
2019-12-27 20:33:09 -0800

    Deploy linear algebras.

--- test/22_test.dart
+++ test/22_test.dart
@@ -17,10 +17,12 @@ void main() {
             yield DealIntoNewStack();
             break;
           case "cut":
-            yield CutN(int.parse(match.group(2)));
+            final n = BigInt.parse(match.group(2));
+            yield CutN(n);
             break;
           case "deal with increment":
-            yield DealWithIncrementN(int.parse(match.group(2)));
+            final n = BigInt.parse(match.group(2));
+            yield DealWithIncrementN(n);
             break;
           default:
             throw ArgumentError("unsupported technique <${match.group(1)}>");
@@ -28,54 +30,59 @@ void main() {
       }
     }
 
-    List<int> shuffle(Iterable<Technique> techniques, List<int> deck) {
-      return techniques.fold(deck, (deck, technique) => technique.apply(deck));
+    List<int> apply(List<Technique> techniques, List<int> deck) {
+      final shuffle = Shuffle.combine(techniques, BigInt.from(deck.length));
+      return deck
+        .map((card) => BigInt.from(card))
+        .map(shuffle.apply)
+        .map((card) => card.toInt())
+        .toList();
     }
 
     test("deal into new stack", () {
-      expect(shuffle([DealIntoNewStack()],
+      expect(apply([DealIntoNewStack()],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
     });
 
     test("deal into new stack, twice", () {
-      expect(shuffle([DealIntoNewStack(), DealIntoNewStack()],
+      expect(apply([DealIntoNewStack(), DealIntoNewStack()],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     });
 
     test("cut n", () {
-      expect(shuffle([CutN(3)],
+      expect(apply([CutN(BigInt.from(3))],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]);
     });
 
     test("cut n, negative", () {
-      expect(shuffle([CutN(-3)],
+      expect(apply([CutN(BigInt.from(-3))],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [7, 8, 9, 0, 1, 2, 3, 4, 5, 6]);
     });
 
     test("deal with increment", () {
-      expect(shuffle([DealWithIncrementN(3)],
+      expect(apply([DealWithIncrementN(BigInt.from(3))],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]);
     });
 
     test("deal with increment, large", () {
-      expect(shuffle([DealWithIncrementN(7)],
+      expect(apply([DealWithIncrementN(BigInt.from(7))],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]);
     });
 
     test("example #1", () {
-      expect(shuffle([DealWithIncrementN(7), DealIntoNewStack(), DealIntoNewStack()],
+      expect(apply([DealWithIncrementN(BigInt.from(7)), DealIntoNewStack(), DealIntoNewStack()],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]);
     });
 
     test("example #2", () {
-      expect(shuffle([CutN(6), DealWithIncrementN(7), DealIntoNewStack()],
+      expect(apply([CutN(BigInt.from(6)), DealWithIncrementN(BigInt.from(7)), DealIntoNewStack()],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
         [3, 0, 7, 4, 1, 8, 5, 2, 9, 6]);
     });
@@ -84,109 +91,79 @@ void main() {
 
     test("Part 1", () {
       var deck = List.generate(10007, (i) => i);
-      deck = shuffle(input, deck);
+      deck = apply(input, deck);
       expect(deck.indexOf(2019), 4775);
     });
 
-    List<int> undoEach(Technique technique, List<int> deck) {
-      return List.generate(deck.length, (i) => technique.undo(deck.length, deck[i]));
-    }
-
-    test("reverse deal into new stack", () {
-      expect(undoEach(DealIntoNewStack(),
-        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    });
-
-    test("reverse cut n", () {
-      expect(undoEach(CutN(3),
-        [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]),
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    });
-
-    test("reverse cut n, negative", () {
-      expect(undoEach(CutN(-3),
-        [7, 8, 9, 0, 1, 2, 3, 4, 5, 6]),
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    });
-
-    test("reverse deal with increment", () {
-      expect(undoEach(DealWithIncrementN(3),
-        [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]),
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    });
-
-    int undo(List<Technique> techniques, int numCards, int position) {
-      return techniques.reversed.fold(position, (position, technique) => technique.undo(numCards, position));
-    }
-
     test("Part 2", () {
-      const repeats = 101741582076661;
-      const numCards = 119315717514047;
-      var position = 2020;
-      for (var i = 0; i < repeats; ++i) {
-        position = undo(input, numCards, position);
-      }
-      expect(position, null);
-    }, skip: true); // too slow!
+      final deckSize = BigInt.from(119315717514047);
+      final position = BigInt.from(2020);
+      final repeats = BigInt.from(101741582076661);
+      final card = Shuffle.combine(input, deckSize).applyN(position, repeats);
+      expect(card, BigInt.from(37889219674304));
+    });
   });
 }
 
+class Shuffle {
+
+  final BigInt deckSize;
+  final BigInt a;
+  final BigInt b;
+
+  Shuffle._(this.deckSize, this.a, this.b);
+
+  static Shuffle combine(List<Technique> techniques, BigInt deckSize) => techniques.reversed.fold(
+    Shuffle._(deckSize, BigInt.one, BigInt.zero),
+    (shuffle, technique) => technique.update(shuffle)
+  );
+
+  Shuffle update(BigInt a, BigInt b) => Shuffle._(deckSize, a % deckSize, b % deckSize);
+
+  BigInt apply(BigInt position) => (a * position + b) % deckSize;
+
+  BigInt applyN(BigInt position, BigInt n) {
+    final an = a.modPow(n, deckSize);
+    final sn = ((an - BigInt.one) * (a - BigInt.one).modInverse(deckSize)) % deckSize;
+    return (an * position + sn * b) % deckSize;
+  }
+}
+
+// ignore: one_member_abstracts
 abstract class Technique {
 
-  List<int> apply(List<int> deck);
-
-  int undo(int numCards, int position);
+  Shuffle update(Shuffle shuffle);
 }
 
 class DealIntoNewStack implements Technique {
 
-  const DealIntoNewStack();
-
   @override
-  List<int> apply(List<int> deck) => deck.reversed.toList();
-
-  @override
-  int undo(int numCards, int position) => numCards - position - 1;
+  Shuffle update(Shuffle shuffle) {
+    return shuffle.update(shuffle.a * -BigInt.one, shuffle.deckSize - BigInt.one - shuffle.b);
+  }
 }
 
 class CutN implements Technique {
 
-  final int _n;
+  final BigInt _n;
 
-  const CutN(this._n);
+  CutN(this._n);
 
   @override
-  List<int> apply(List<int> deck) {
-    if (_n > 0) {
-      deck = deck.sublist(_n) + deck.sublist(0, _n);
-    } else if (_n < 0) {
-      deck = deck.sublist(deck.length + _n) + deck.sublist(0, deck.length + _n);
-    }
-    return deck;
+  Shuffle update(Shuffle shuffle) {
+    return shuffle.update(shuffle.a, shuffle.b + _n);
   }
-
-  @override
-  int undo(int numCards, int position) => (position - _n) % numCards;
 }
 
 class DealWithIncrementN implements Technique {
 
-  final int _n;
+  final BigInt _n;
 
-  const DealWithIncrementN(this._n);
-
-  @override
-  List<int> apply(List<int> deck) {
-    final shuffled = List.filled(deck.length, 0);
-    for (var i = 0; i < deck.length; ++i) {
-      shuffled[(i * _n) % deck.length] = deck[i];
-    }
-    return shuffled;
-  }
+  DealWithIncrementN(this._n);
 
   @override
-  int undo(int numCards, int position) {
-    return position * _n % numCards;
+  Shuffle update(Shuffle shuffle) {
+    final i = _n.modInverse(shuffle.deckSize);
+    return shuffle.update(shuffle.a * i, shuffle.b * i);
   }
 }
